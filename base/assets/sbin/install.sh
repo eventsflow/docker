5fa0482e1af61cbd69222c4613dc3086103f8a0a
ownport <ownport@gmail.com>
2021-01-01 16:42:57 +0200

    refact: check user before run install script

--- base/assets/sbin/install.sh
+++ base/assets/sbin/install.sh
@@ -2,6 +2,12 @@
 
 set -e
 
+if [ "$(id -u)" -ne 0 ]; then
+    echo -e 'Script must be run as root. Use sudo, su, or add "USER root" to your Dockerfile before running this script.'
+    exit 1
+fi
+
+
 export DEBIAN_FRONTEND=noninteractive
 
 echo "[INFO] Update apt index and upgrade to latest versions for core components" && \
