bede00923538329a49c7a17f0b2816ab11e87192
ownport <ownport@gmail.com>
2021-01-01 16:46:15 +0200

    feat: create user

--- dev-vscode/assets/sbin/dev-vscode-install.sh
+++ dev-vscode/assets/sbin/dev-vscode-install.sh
@@ -2,3 +2,24 @@
 
 set -e
 
+# Create or update a non-root user to match UID/GID.
+if id -u ${USERNAME} > /dev/null 2>&1; then
+    # User exists, update if needed
+    if [ "$USER_GID" != "$(id -G $USERNAME)" ]; then 
+        groupmod --gid $USER_GID $USERNAME 
+        usermod --gid $USER_GID $USERNAME
+    fi
+    if [ "$USER_UID" != "$(id -u $USERNAME)" ]; then 
+        usermod --uid $USER_UID $USERNAME
+    fi
+else
+    # Create user
+    groupadd --gid $USER_GID $USERNAME
+    useradd -s /bin/bash --uid $USER_UID --gid $USERNAME -m $USERNAME
+fi
+
+# Add add sudo support for non-root user
+if [ "${USERNAME}" != "root" ]; then
+    echo $USERNAME ALL=\(root\) NOPASSWD:ALL > /etc/sudoers.d/$USERNAME
+    chmod 0440 /etc/sudoers.d/$USERNAME
+fi
